3129100ea4e06a477801e251b1a8498e4f172f38
Gautam Diya <gautamdiya25@gmail.com>
2024-07-28 17:22:10 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
+import java.time.Duration;
 
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
@@ -30,7 +32,9 @@ public class TestCases {
     @Test
     public void testCase01() throws InterruptedException {
         driver.get("https://www.flipkart.com");
-        Thread.sleep(5000);
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+      wait.until(ExpectedConditions.urlContains("flipkart"));
+
         // wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'✕')]"))).click();
         
         WebElement searchBox = driver.findElement(By.name("q"));
@@ -41,13 +45,20 @@ public class TestCases {
         popularityElement.click();
         Thread.sleep(5000);
         
-        List<WebElement> ratings = driver.findElements(By.cssSelector("div._3LWZlK"));
-        long count = ratings.stream()
-                .filter(r -> {
-                    String text = r.getText();
-                    return !text.isEmpty() && Double.parseDouble(text) <= 4.0;
-                })
-                .count();
+        List<WebElement> ratings = driver.findElements(By.xpath("//div[@class='XQDdHH']"));
+        // long count = ratings.stream()
+        //         .filter(r -> {
+        //             String text = r.getText();
+        //             return !text.isEmpty() && Double.parseDouble(text) <= 4.0;
+        //         })
+        //         .count();
+        int count=0;
+        for(WebElement element:ratings){
+            double rating=Double.parseDouble(element.getText().trim());
+            if(rating<=4){
+                count++;
+            }
+        }
         
         System.out.println("Count of items with rating <= 4 stars: " + count);
     }
@@ -55,17 +66,22 @@ public class TestCases {
     @Test
     public void testCase02() throws InterruptedException {
         driver.get("https://www.flipkart.com");
-        Thread.sleep(5000);
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+      wait.until(ExpectedConditions.urlContains("flipkart"));
         
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("iPhone");
         searchBox.submit();
-        
-        List<WebElement> items = driver.findElements(By.cssSelector("a._1fQZEK"));
+
+        Thread.sleep(5000);
+        List<WebElement> items = driver.findElements(By.xpath("//div[@class='tUxRFH']"));
+        Thread.sleep(5000);
         for (WebElement item : items) {
-            String discountText = item.findElement(By.cssSelector("div._3Ay6Sb span")).getText().replaceAll("[^0-9]", "");
-            if (!discountText.isEmpty() && Integer.parseInt(discountText) > 17) {
-                String title = item.findElement(By.cssSelector("div._4rR01T")).getText();
+            String discountText = item.findElement(By.xpath(".//div[@class='UkUFwK']")).getText().replaceAll("[^0-9]", "");
+            // System.out.println(discountText);
+            Integer Discount=Integer.parseInt(discountText);
+            if (Discount > 17) {
+                String title = item.findElement(By.xpath(".//div[@class='KzDlHZ']")).getText();
                 System.out.println("Title: " + title + ", Discount: " + discountText + "%");
             }
         }
@@ -74,34 +90,131 @@ public class TestCases {
     @Test
     public void testCase03() throws InterruptedException {
         driver.get("https://www.flipkart.com");
-        Thread.sleep(5000);
-        
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("Coffee Mug");
-        searchBox.submit();
-        
-        WebElement filter=driver.findElement(By.xpath("//div[contains(text(),'4★ & above')]"));
-        filter.click();
-        Thread.sleep(5000);
-        
-        List<WebElement> items = driver.findElements(By.cssSelector("a._1fQZEK"));
-        List<WebElement> topItems = items.stream()
-                .sorted((item1, item2) -> {
-                    int reviews1 = Integer.parseInt(item1.findElement(By.cssSelector("span._2_R_DZ span")).getText().replaceAll("[^0-9]", ""));
-                    int reviews2 = Integer.parseInt(item2.findElement(By.cssSelector("span._2_R_DZ span")).getText().replaceAll("[^0-9]", ""));
-                    return reviews2 - reviews1;
-                })
-                .limit(5)
-                .collect(Collectors.toList());
-        
-        for (WebElement item : topItems) {
-            String title = item.findElement(By.cssSelector("div._4rR01T")).getText();
-            String imageUrl = item.findElement(By.cssSelector("img")).getAttribute("src");
-            System.out.println("Title: " + title + ", Image URL: " + imageUrl);
+    Thread.sleep(5000);
+    
+    // Search for Coffee Mug
+    WebElement searchBox = driver.findElement(By.name("q"));
+    searchBox.sendKeys("Coffee Mug");
+    searchBox.submit();
+    Thread.sleep(5000);
+    // Filter by 4 stars and above
+    WebElement fourStarsAndAboveFilter = driver.findElement(By.xpath("//div[contains(text(),'4★ & above')]"));
+    fourStarsAndAboveFilter.click();
+    
+    Thread.sleep(2000);
+    // Fetch all items
+    List<WebElement> items = driver.findElements(By.xpath("//div[@class='slAVV4']"));
+    
+    // Create a list to store items with their reviews count
+    List<Item> itemList = new ArrayList<>();
+    
+    for (WebElement item : items) {
+        try {
+            // Find the number of reviews
+            WebElement reviewsElement = item.findElement(By.xpath(".//span[@class='Wphh3N']"));
+            String reviewsText = reviewsElement.getText().split(" ")[0].replaceAll("[^0-9]", "");
+            int reviewsCount = Integer.parseInt(reviewsText);
             
+            // Find the item title
+            WebElement titleElement = item.findElement(By.xpath(".//a[@class='wjcEIp']"));
+            String title = titleElement.getText();
+            
+            // Find the image URL
+            WebElement imageElement = item.findElement(By.cssSelector("img"));
+            String imageUrl = imageElement.getAttribute("src");
+            
+            // Add to the list
+            itemList.add(new Item(title, imageUrl, reviewsCount));
+        } catch (Exception e) {
+            System.out.println("An item did not have the expected elements or had a format issue.");
         }
+    }
+    
+    // Sort the items by reviews count in descending order and get the top 5
+    itemList.sort((a, b) -> Integer.compare(b.getReviewsCount(), a.getReviewsCount()));
+    List<Item> topItems = itemList.stream().limit(5).collect(Collectors.toList());
+    
+    // Print the title and image URL of the top 5 items
+    for (Item item : topItems) {
+        System.out.println("Title: " + item.getTitle() + ", Image URL: " + item.getImageUrl());
+    }
+    class Item {
+    private String title;
+    private String imageUrl;
+    private int reviewsCount;
+    
+    public Item(String title, String imageUrl, int reviewsCount) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.reviewsCount = reviewsCount;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    
+    public int getReviewsCount() {
+        return reviewsCount;
+    }
+}
 
-        }
+}
+class Item {
+    private String title;
+    private String imageUrl;
+    private int reviewsCount;
+    
+    public Item(String title, String imageUrl, int reviewsCount) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.reviewsCount = reviewsCount;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    
+    public int getReviewsCount() {
+        return reviewsCount;
+    }
+}
+    //     driver.get("https://www.flipkart.com");
+    //     WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+    //   wait.until(ExpectedConditions.urlContains("flipkart"));
+        
+    //     WebElement searchBox = driver.findElement(By.name("q"));
+    //     searchBox.sendKeys("Coffee Mug");
+    //     searchBox.submit();
+        
+    //     WebElement filter=driver.findElement(By.xpath("//div[contains(text(),'4★ & above')]"));
+    //     filter.click();
+    //     Thread.sleep(5000);
+        
+    //     List<WebElement> items = driver.findElements(By.cssSelector("a._1fQZEK"));
+    //     List<WebElement> topItems = items.stream()
+    //             .sorted((item1, item2) -> {
+    //                 int reviews1 = Integer.parseInt(item1.findElement(By.cssSelector("span._2_R_DZ span")).getText().replaceAll("[^0-9]", ""));
+    //                 int reviews2 = Integer.parseInt(item2.findElement(By.cssSelector("span._2_R_DZ span")).getText().replaceAll("[^0-9]", ""));
+    //                 return reviews2 - reviews1;
+    //             })
+    //             .limit(5)
+    //             .collect(Collectors.toList());
+        
+    //     for (WebElement item : topItems) {
+    //         String title = item.findElement(By.cssSelector("div._4rR01T")).getText();
+    //         String imageUrl = item.findElement(By.cssSelector("img")).getAttribute("src");
+    //         System.out.println("Title: " + title + ", Image URL: " + imageUrl);
+            
+    //     }
+
      
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
